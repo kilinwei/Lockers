99028f9fa255d0beb65e5b784d0bfc4eaabfa4e7
kilin <kilinwei@gmail.com>
2019-04-17 15:37:04 +0800

    优化临时存取界面

--- app/src/main/java/com/xyf/lockers/ui/activity/TemporaryTakeActivity.java
+++ app/src/main/java/com/xyf/lockers/ui/activity/TemporaryTakeActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.baidu.idl.facesdk.model.Feature;
 import com.xyf.lockers.R;
@@ -51,6 +52,8 @@ public class TemporaryTakeActivity extends BaseActivity implements ILivenessCall
     RelativeLayout mCameraView;
     @BindView(R.id.image_track)
     ImageView mImageTrack;
+    @BindView(R.id.tv_similarity)
+    TextView mTvSimilarity;
     private Context mContext;
     private BinocularView mBinocularView;
     private MonocularView mMonocularView;
@@ -155,7 +158,10 @@ public class TemporaryTakeActivity extends BaseActivity implements ILivenessCall
 
     @Override
     public void onTip(int code, String msg) {
-
+        if (mTvSimilarity != null) {
+            mTvSimilarity.setText(msg);
+        }
+        Log.i(TAG, "onTip: " + msg);
     }
 
     @Override
@@ -169,6 +175,9 @@ public class TemporaryTakeActivity extends BaseActivity implements ILivenessCall
             //匹配到相似人脸,说明这个人已经存过东西,检测已经存几个,将用户存的箱子一次性打开
             //相似度
             float featureScore = livenessModel.getFeatureScore();
+            if (mTvSimilarity != null) {
+                mTvSimilarity.setText(String.format("相似度: %s", featureScore));
+            }
             if (featureScore < Constants.PASS_SCORE) {
                 return;
             }
@@ -210,6 +219,10 @@ public class TemporaryTakeActivity extends BaseActivity implements ILivenessCall
                 //说明facesdk的数据库里有数据,但是user数据库没有,说明user已被删除,没有存东西,不需要处理
             }
         } else {
+            Log.i(TAG, "run: 未匹配到相似人脸");
+            if (mTvSimilarity != null) {
+                mTvSimilarity.setText("未匹配到相似人脸");
+            }
             Log.i(TAG, "run: 未匹配到相似人脸");
         }
     }
